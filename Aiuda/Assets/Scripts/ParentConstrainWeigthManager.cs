cd7965d4242c26343eb6b2d6226cea44e3e5474b
LMMoonlight <lauramolinay@gmail.com>
2022-11-12 21:14:38 -0500

    Commit ante de que algo salga mal
    porque algo va a salir mal

---
+++ Aiuda/Assets/Scripts/ParentConstrainWeigthManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class ParentConstrainWeigthManager : MonoBehaviour
+{
+    [SerializeField] [Range(0,1)] private float weight;
+    [SerializeField] private MultiParentConstraint[] targetChain;
+
+    public void SetConstrainsWeights()
+    {
+        if (targetChain == null || targetChain.Length <= 0) return;
+
+        for (int i = 0; i < targetChain.Length; i++)
+        {
+            targetChain[i].weight = weight;
+        }
+    }
+
+    private void OnValidate()
+    {
+        SetConstrainsWeights();
+    }
+}
